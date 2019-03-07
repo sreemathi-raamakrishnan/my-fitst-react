29750dfea3606abc5c4bd0d420c2dcc94e92f262
sreemathi-raamakrishnan <48316478+sreemathi-raamakrishnan@users.noreply.github.com>
2019-03-07 16:18:05 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import App from './index.js';
+export default class FirstComponent extends Component {
+/*render() {
+    return (<div className="offer-code">
+    <div id="cart-value"></div>
+    <h3>Apply offer code</h3>
+    <input type="text" class="offer-code__textbox" id="offer-code__textbox" placeholder="Enter offer code"/>
+    </div>);
+  }*/
+  render() {
+    <div id="root"></div>
+  }
+}
