e59d06febb485ac319d4e0c802cc69bbb0d36042
sreemathi-raamakrishnan <48316478+sreemathi-raamakrishnan@users.noreply.github.com>
2019-03-07 16:19:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-	  this.state = {
-         data: 'Initial data...'
-      }
-    this.updateState = this.updateState.bind(this);
-  };
-   updateState(e) {
-      this.setState({data: e.target.value});
-   }
-   render() {
-      return (
-         <div>
-            <input type = "text" value = {this.state.data} 
-               onChange = {this.updateState} />
-            <h4>{this.state.data}</h4>
-         </div>
-      );
-   }
-}
-export default App;
-ReactDOM.render(<App/>, document.getElementById('root'));
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
