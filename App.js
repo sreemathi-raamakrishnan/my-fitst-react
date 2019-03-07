340229b5634cb75e5b60bf6f5fbf16cb4a4f5647
sreemathi-raamakrishnan <48316478+sreemathi-raamakrishnan@users.noreply.github.com>
2019-03-07 16:19:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React, {Component} from 'react';
-import App from './index.js';
-export default class FirstComponent extends Component {
-/*render() {
-    return (<div className="offer-code">
-    <div id="cart-value"></div>
-    <h3>Apply offer code</h3>
-    <input type="text" class="offer-code__textbox" id="offer-code__textbox" placeholder="Enter offer code"/>
-    </div>);
-  }*/
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header.jsx';
+import Maincontent from './Maincontent.jsx';
+import Footer from './Footer.jsx';
+
+class App extends Component {
   render() {
-    <div id="root"></div>
+    return (
+      <div className="App">
+        <Header />
+        <Maincontent />
+        <Footer />
+      </div>
+    );
   }
 }
+
+export default App;
+
