3f179d3fa95a1466ba5745bcd50d177da7940c5e
sreemathi-raamakrishnan <48316478+sreemathi-raamakrishnan@users.noreply.github.com>
2019-03-07 16:20:54 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+       <ul>
+         <li> Privacy policy </li>
+         <li> Terms & Conditions </li>
+       </ul>
+      </div>
+    );
+  }
+}
+
+export default Footer;
